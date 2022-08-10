c6c7613e3e49b97f6b1b18d7aeaf3a19eb7e511e
Burak AÇIK <burakacik@outlook.com>
2022-08-10 12:44:46 +0300

    UIAlertController ile Listeye Eleman Ekleme // Ders Sonu

---
+++ PatikaListApp/Controllers/ViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  PatikaListApp
+//
+//  Created by Burak AÇIK on 9.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data = [String]()
+    var alertController = UIAlertController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+
+    @IBAction func didRemoveBarButtonItemTapped(_ sender: Any) {
+        presentAlert(title: "Uyarı!",
+                     message: "Listedeki bütün öğeleri silmek istediğinize emin misiniz?",
+                     defaultButtonTitle: "Evet",
+                     cancelButtonTitle: "Vazgeç") { _ in
+            self.data.removeAll()
+            self.tableView.reloadData()
+        }
+    }
+    
+    @IBAction func didAddBarButtonItemTapped(_ sender: Any) {
+        presentAddAlert()
+    }
+    
+    
+    func presentAddAlert() {
+        presentAlert(title: "Yeni Eleman Ekle",
+                     message: nil,
+                     defaultButtonTitle: "Ekle",
+                     cancelButtonTitle: "Vazgeç",
+                     isTextFieldAvailable: true,
+                     defaultButtonHandler: { _ in
+            let text = self.alertController.textFields?.first?.text
+            if text != "" {
+                self.data.append((text)!)
+                self.tableView.reloadData()
+            } else {
+                self.presentWarningAlert()
+            }
+        })
+    }
+    
+    
+    func presentWarningAlert() {
+        presentAlert(title: "Uyarı!",
+                     message: "Liste eleman boş olamaz",
+                     cancelButtonTitle: "Tamam")
+    }
+    
+    
+    func presentAlert(title: String?,
+                      message: String?,
+                      preferredStyle: UIAlertController.Style = .alert,
+                      defaultButtonTitle: String? = nil,
+                      cancelButtonTitle: String?,
+                      isTextFieldAvailable: Bool = false,
+                      defaultButtonHandler: ((UIAlertAction) -> Void)? = nil) {
+        
+        alertController = UIAlertController(title: title,
+                                            message: message,
+                                            preferredStyle: preferredStyle)
+        
+        if defaultButtonTitle != nil {
+            let defaultButton = UIAlertAction(title: defaultButtonTitle,
+                                              style: .default,
+                                              handler: defaultButtonHandler)
+            alertController.addAction(defaultButton)
+        }
+        
+        let cancelButton = UIAlertAction(title: cancelButtonTitle,
+                                         style: .cancel)
+        
+        if isTextFieldAvailable {
+            alertController.addTextField()
+        }
+        
+        alertController.addAction(cancelButton)
+        
+        present(alertController, animated: true)
+    }
+    
+    
+}
